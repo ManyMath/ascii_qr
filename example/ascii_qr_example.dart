e99d0a67904a4be005950d1b40275656488e2b80
sneurlax <sneurlax@gmail.com>
2024-12-05 21:34:43 -0600

    chore: dart create -t package ascii_qr

---
+++ example/ascii_qr_example.dart
@@ -0,0 +1,6 @@
+import 'package:ascii_qr/ascii_qr.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/ascii_qr_test.dart
@@ -0,0 +1,16 @@
+import 'package:ascii_qr/ascii_qr.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
